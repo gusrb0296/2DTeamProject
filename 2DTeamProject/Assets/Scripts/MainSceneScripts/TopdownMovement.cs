f267ace31159af6106deed2d40e03ef47e512e3e
wkdtjdrb_35 <81483103+zjarhdtjdrb@users.noreply.github.com>
2023-11-30 15:57:10 +0900

    character move

---
+++ 2DTeamProject/Assets/Scripts/MainSceneScripts/TopdownMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TopdownMovement : MonoBehaviour
+{
+    private TopDownCharacterController _controller;
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementDirection = Vector2.zero;
+    private float x;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move; // ±¸µ¶
+    }
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+    private void ApplyMovent(Vector2 direction)
+    {
+        direction = direction * 5;
+        _rigidbody.velocity = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovent(_movementDirection);
+        LookRotate();
+        CollisionWithWall();
+    }
+    private void LookRotate()
+    {
+        if (_movementDirection.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (_movementDirection.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+    private void CollisionWithWall()
+    {
+        x = transform.position.x;
+        if (x < -8.3f)
+        {
+            x = -8.3f;
+        }
+        if (x > 8.3f)
+        {
+            x = 8.3f;
+        }
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
