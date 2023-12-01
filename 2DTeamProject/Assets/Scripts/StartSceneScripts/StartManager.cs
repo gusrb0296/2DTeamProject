4fe3cd234351c126d07768ab141ccbb4e14cd89f
박정혁 <146924502+jeonghyeok2@users.noreply.github.com>
2023-12-01 15:01:37 +0900

    Feat : StartScene 추가
    - 설명창 추가
    - 설정창에서 오디오 소리 설정 가능, 및 음소거 기능 추가
    - 게임 시작시 레벨 선택 창 추가

--- 2DTeamProject/Assets/Scripts/StartSceneScripts/StartManager.cs
+++ 2DTeamProject/Assets/Scripts/StartSceneScripts/StartManager.cs
@@ -7,26 +7,73 @@ using UnityEngine.UI;
 
 public class StartManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _gameSetting;
+    [SerializeField] private GameObject _Canvas;
+    [SerializeField] private GameObject _muteOn;
+    [SerializeField] private GameObject _muteOff;
+    [SerializeField] private GameObject _Guide;
+    [SerializeField] private Button _gameSettingBtn;
     [SerializeField] private Button _startBtn;
     [SerializeField] private Button _closeBtn;
+    [SerializeField] private Button _easyBtn;
+    [SerializeField] private Button _normalBtn;
+    [SerializeField] private Button _hardBtn;
+    [SerializeField] private Button _guideCloseBtn;
+
+
+
+    public Slider _soundSlider;
+    public AudioSource _audioSource;
 
     private void Awake()
     {
-        Button _gameSettingBtn = _gameSetting.GetComponent<Button>();
+        Button _muteOnBtn = _muteOn.GetComponent<Button>();
+        Button _muteOffBtn = _muteOff.GetComponent<Button>();
+        Button _guideOpenBtn = _Guide.transform.parent.GetComponent<Button>();
+        _audioSource = GetComponent<AudioSource>();
 
-        _startBtn.onClick.AddListener(() => LoadScene());
+        _startBtn.onClick.AddListener(() => LevelWindow());
         _closeBtn.onClick.AddListener(() => SettingWindow(false));
         _gameSettingBtn.onClick.AddListener(() => SettingWindow(true));
+        _easyBtn.onClick.AddListener(() => LoadScene(0));
+        _normalBtn.onClick.AddListener(() => LoadScene(1));
+        _hardBtn.onClick.AddListener(() => LoadScene(2));
+        _muteOnBtn.onClick.AddListener(() => MuteSystem(true, false));
+        _muteOffBtn.onClick.AddListener(() => MuteSystem(false, true));
+        _guideOpenBtn.onClick.AddListener(() => GuideWindow(true));
+        _guideCloseBtn.onClick.AddListener(() => GuideWindow(false));
     }
 
+
+    private void Update()
+    {
+        SoundSystem(_soundSlider.value);
+    }
+
+    private void GuideWindow(bool istrue)
+    {
+        _Guide.gameObject.SetActive(istrue);
+    }
+    private void SoundSystem(float value)
+    {
+        _audioSource.volume = value;
+    }
+    private void MuteSystem(bool isMuteOn, bool isMuteOff)
+    {
+        _audioSource.mute = isMuteOn;
+        _muteOn.transform.GetChild(0).gameObject.SetActive(isMuteOn);
+        _muteOff.transform.GetChild(0).gameObject.SetActive(isMuteOff);
+    }
     private void SettingWindow(bool istrue)
     {
-        _gameSetting.transform.GetChild(1).gameObject.SetActive(istrue);
+        _Canvas.transform.Find("SettingBackGround").gameObject.SetActive(istrue);
     }
-
-    private void LoadScene()
+    private void LevelWindow()
+    {
+        _Canvas.transform.Find("LevelBackGround ").gameObject.SetActive(true);
+    }
+    private void LoadScene(int index)
     {
         SceneManager.LoadScene("MainScene");
+        //게임 매니져한테 index값 넘겨주면 해당하는 Enum값 설정하게
     }
 }
