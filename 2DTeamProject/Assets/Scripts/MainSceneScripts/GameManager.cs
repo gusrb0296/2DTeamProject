134b9ada7b3845c895d6612a127d46a2f77b1629
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-12-05 17:17:46 +0900

    Merge pull request #14 from gusrb0296/Main_jeonghyeok
    Feat : 점수 추가

---
+++ 2DTeamProject/Assets/Scripts/MainSceneScripts/GameManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject _BallPrefabs;
+    public UIManager _uiManager;
+
+    public float _time; //시간저장
+
+    int _GameLevelIndex;
+    int _ballCount;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        Time.timeScale = 1;
+        _GameLevelIndex = PlayerPrefs.GetInt("GameLevel");
+    }
+
+    private void Start()
+    {
+        BallPorduce();
+        InvokeRepeating("BallSpawn", 5, 7);
+    }
+    private void Update()
+    {
+        _uiManager.GameOver(); //update문에다 사용시 너무 낭비되니깐 공피격시로 옮기기
+        _time += Time.deltaTime;
+    }
+
+    public void BallPorduce() // 공생성
+    {
+        for (int i = 0; i < _GameLevelIndex; i++)
+        {
+            Instantiate(_BallPrefabs);
+        }
+    }
+    public void BallSpawn()
+    {
+        Instantiate(_BallPrefabs);
+        _ballCount++;
+
+        if (_ballCount == _GameLevelIndex)
+        {
+            CancelInvoke("BallSpawn");
+        }
+        else if (_ballCount == _GameLevelIndex)
+        {
+            CancelInvoke("BallSpawn");
+        }
+        else if(_ballCount == _GameLevelIndex)
+        {
+            CancelInvoke("BallSpawn");
+        }
+    } //난이도에 따른 공생성 갯수
+}
