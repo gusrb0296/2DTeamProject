f914347b64cde0604dcf02e093c8fe0e620b2344
GreatJun <ljun970@naver.com>
2023-12-05 21:06:30 +0900

    fix : Bounce 수정 및 Player 스크립트 추가
    1. Bounce ball 에 Collision 경우 추가
    
    2. Player오브젝트에 PlayerItemState스크립트 컴포넌트 추가

--- 2DTeamProject/Assets/Scripts/MainSceneScripts/Items/ItemPrefabsLogic/BounceBulletPrefabLogic.cs
+++ 2DTeamProject/Assets/Scripts/MainSceneScripts/Items/ItemPrefabsLogic/BounceBulletPrefabLogic.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BounceBulletPrefabLogic : ItemManager
+public class BounceBulletPrefabLogic : MonoBehaviour
 {
+    PlayerItemState _itemManager;
+
     const float _Radian = 180f;
     bool firstWallCheck = true;
     Rigidbody2D _rigid;
@@ -15,6 +17,7 @@ public class BounceBulletPrefabLogic : ItemManager
     private void Awake()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _itemManager = FindObjectOfType<PlayerItemState>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,12 +26,15 @@ public class BounceBulletPrefabLogic : ItemManager
         {
             if (firstWallCheck == true)
             {
+                // byllet CoolTime Reset
+                _itemManager.BulletCoolTimeReset();
+
                 // Random Angle Setting
                 int randomAngle = Random.Range(0, arrAngles.Length);
                 Vector3 tmps = transform.eulerAngles;
                 tmps.z = arrAngles[randomAngle];
                 transform.eulerAngles = tmps;
-                _rigid.AddForce(transform.up * Force, ForceMode2D.Impulse);
+                _rigid.AddForce(transform.up * _itemManager._player.Force, ForceMode2D.Impulse);
                 firstWallCheck = false;
             }
             else
@@ -38,24 +44,21 @@ public class BounceBulletPrefabLogic : ItemManager
                 tmp.z = _Radian - tmp.z;
                 transform.eulerAngles = tmp;
                 // Bounce After AddForce
-                _rigid.AddForce(transform.up * Force, ForceMode2D.Impulse);
+                _rigid.AddForce(transform.up * _itemManager._player.Force, ForceMode2D.Impulse);
             }
         }
         else if (collision.collider.CompareTag("Wall"))
         {
-            // byllet CoolTime Reset
-            BulletCoolTimeReset();
-
             Vector3 tmp = transform.eulerAngles;
             tmp.z = (_Radian * 2) - tmp.z;
             transform.eulerAngles = tmp;
-            _rigid.AddForce(transform.up * Force, ForceMode2D.Impulse);
+            _rigid.AddForce(transform.up * _itemManager._player.Force, ForceMode2D.Impulse);
         }
         bulletLifeCount++;
 
         if (collision.collider.CompareTag("Ball"))
         {
-            BulletCoolTimeReset();
+            _itemManager.BulletCoolTimeReset();
             Destroy(gameObject);
         }
 
@@ -65,4 +68,13 @@ public class BounceBulletPrefabLogic : ItemManager
             Destroy(gameObject);
         }
     }
+
+    //private void ontriggerenter2d(collider2d collision)
+    //{
+    //    if (collision.tag == "ball")
+    //    {
+    //        _itemmanager.bulletcooltimereset();
+    //        destroy(gameobject);
+    //    }
+    //}
 }
