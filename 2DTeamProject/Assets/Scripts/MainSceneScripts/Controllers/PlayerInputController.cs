15c9d477a87d8961a5abc0ef192876afe5cf2a16
wkdtjdrb_35 <81483103+zjarhdtjdrb@users.noreply.github.com>
2023-12-06 19:41:34 +0900

    대쉬 이펙트 효과 추가

--- 2DTeamProject/Assets/Scripts/MainSceneScripts/Controllers/PlayerInputController.cs
+++ 2DTeamProject/Assets/Scripts/MainSceneScripts/Controllers/PlayerInputController.cs
@@ -6,8 +6,11 @@ using UnityEngine.InputSystem;
 public class PlayerInputController : TopDownCharacterController
 {
     [SerializeField] private Animator _anim;
-    [SerializeField] private SpriteRenderer _spritexplosion;
-    [SerializeField] private float speed;
+    [SerializeField] private SpriteRenderer _explosionSprite;
+    [SerializeField] private SpriteRenderer _playerSprite;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float _dashDuration = 0.2f;  // 대시와 이펙트의 지속 시간은 동일하므로 같은 변수 사용
+    [SerializeField] private float _dashFadeSpeed = 5f;  // 대시 이펙트의 투명도 감소 속도
     private Vector2 playerMoveDir;
     private PlayerAttackSystem _playerAttackSystem;
     private bool coolTimeCheckValue;
@@ -36,7 +39,7 @@ public class PlayerInputController : TopDownCharacterController
             if (coolTimeCheckValue == true)
             {
                 _anim.SetTrigger("Attack");
-                StartCoroutine(ShowAndHide(_spritexplosion, 0.1f));
+                StartCoroutine(ShowAndHide(_explosionSprite, 0.1f));
             }
         }
     }
@@ -50,9 +53,11 @@ public class PlayerInputController : TopDownCharacterController
     public void OnDash()
     {
         if (Input.GetKeyDown(KeyCode.LeftControl))
-            StartCoroutine(Dash(0.2f));
+        {
+            StartCoroutine(Dash(_dashDuration));
+        }
     }
-    private IEnumerator Dash(float dashTime)
+    private IEnumerator Dash(float dashTime) // 대쉬 기능
     {
         Vector3 dashDirection = playerMoveDir.x > 0 ? Vector3.right : Vector3.left;
         float startTime = Time.time;
@@ -60,7 +65,29 @@ public class PlayerInputController : TopDownCharacterController
         while (Time.time < startTime + dashTime)
         {
             transform.position += dashDirection * speed * Time.deltaTime; // 대쉬 속도를 고정하고 방향에 따라 이동
+            StartCoroutine(DashEffect(_playerSprite, _dashDuration, _dashFadeSpeed));  // 대시 이펙트 생성
             yield return null;
         }
+    }
+    private IEnumerator DashEffect(SpriteRenderer sprite, float dashEffectTime, float dashEffectfadeSpeed)
+    {
+        // 스프라이트 복사본 생성
+        GameObject ghost = new GameObject();
+        SpriteRenderer ghostSprite = ghost.AddComponent<SpriteRenderer>();
+        ghostSprite.sprite = sprite.sprite;
+        ghost.transform.position = transform.position;
+        ghost.transform.rotation = transform.rotation;
+        ghost.transform.localScale = transform.localScale;
+
+        // 일정 시간 동안 복사본을 보이게 한 후 투명도를 점차 감소시킴
+        float startTime = Time.time;
+        while (Time.time < startTime + dashEffectTime)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, (Time.time - startTime) * dashEffectfadeSpeed); // 투명도 감소
+            ghostSprite.color = new Color(1f, 1f, 1f, alpha);
+            yield return null;
+        }
+        // 복사본 제거
+        Destroy(ghost);
     }
 }
